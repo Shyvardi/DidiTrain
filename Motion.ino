138cabc53cec50a6d8adbcb5f80dfbde801bb4f6
Shyvardi <56295870+Shyvardi@users.noreply.github.com>
2024-09-21 12:13:04 +0300

    Add files via upload

---
+++ Motion.ino
@@ -0,0 +1,65 @@
+void motion() {
+  digitalWrite(enablePin, HIGH);
+
+  // תזוזה של מנוע הצעד למעלה (1200 צעדים)
+  if (myServo.read() == 0) {
+    moveStepperToPosition(1200, "going up");
+
+    // בדיקה אם עזבה את המפסק גבול התחתון אחרי 2 שניות
+    delay(2000);
+    if (digitalRead(lowerLimitPin) == LOW) {
+      Serial.println("Error: Carriage did not leave the bottom limit switch.");
+      stopProgram();  // פונקציה שעוצרת את התוכנית
+      return;  // יציאה מהפונקציה
+    }
+  }
+
+  // סיבוב הסרוו מ-0 ל-128 מעלות
+  rotateServo(0, 128, 10);
+
+  // תזוזה של מנוע הצעד חזרה למטה (ל-0 צעדים)
+  if (myServo.read() == 128) {
+    moveStepperToPosition(0, "Going down");
+  }
+
+  // סיבוב הסרוו חזרה מ-128 ל-0 מעלות
+  rotateServo(128, 0, 10);
+   delay(500);
+   if (digitalRead(lowerLimitPin) == HIGH) {
+    Serial.println("Error: Carriage did not return to the bottom limit switch.");
+  }
+
+
+
+  programStarted = false; // תסמן שהתוכנית הסתיימה
+  Serial.println("Program stopped. Press button to restart.");
+}
+
+// פונקציה לסיבוב סרוו
+void rotateServo(int startAngle, int endAngle, int delayTime) {
+  if (startAngle < endAngle) {
+    for (int angle = startAngle; angle <= endAngle; angle++) {
+      myServo.write(angle);
+      delay(delayTime);
+    }
+  } else {
+    for (int angle = startAngle; angle >= endAngle; angle--) {
+      myServo.write(angle);
+      delay(delayTime);
+    }
+  }
+}
+
+void moveStepperToPosition(int position, const char* message) {
+  Serial.println(message);
+  stepper.runToNewPosition(position);
+  delay(1000);  // השהיה בין התנועות
+}
+
+// פונקציה שעוצרת את התוכנית
+void stopProgram() {
+  Serial.println("Stopping program due to error.");
+  while (true) {
+    // לולאה אינסופית שתעצור את התוכנית
+  }
+}
