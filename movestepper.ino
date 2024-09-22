3210f667ec084d1dad4910238070fd143429e5ba
Shyvardi <56295870+Shyvardi@users.noreply.github.com>
2024-09-22 08:24:19 +0300

    Update movestepper.ino

--- movestepper.ino
+++ movestepper.ino
@@ -5,7 +5,9 @@ void movestepper (long Position, const char* message ){
     stepper.moveTo(Position);
     while (stepper.distanceToGo() != 0) {  // נמשיך להריץ את המנוע עד שהוא יגיע ליעד
     if (stepper.distanceToGo() <= Position-200 && digitalRead(lowerLimitPin) == LOW ) {
-       stopProgram();
+       Serial.println("לא זז מהמקום");
+       
+       //stopProgram();
        }
     stepper.run();
       }
@@ -14,6 +16,11 @@ void movestepper (long Position, const char* message ){
   while (stepper.distanceToGo() != 0) {  // נמשיך להריץ את המנוע עד שהוא יגיע ליעד
     stepper.run();
   }
+  if (digitalRead(lowerLimitPin) == HIGH ){
+    Serial.println("לא הגיע למקום");
+    
+    //stopProgram();
+     }
   }
 
 
